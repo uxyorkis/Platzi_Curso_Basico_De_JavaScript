4782e5e5b6713991688759429df3f4f5b5948e3a
Zeus <j.guerrero.morla@gmail.com>
2022-02-06 14:26:13 -0400

    Se sube el archivo valores.js

---
+++ valores.js
@@ -0,0 +1,15 @@
+40 //Este valor es un numero
+
+"Esto es una entrada de texto 🎉" //Este valor es un string
+
+true // Este es un valor buleano
+false // Este es un valor buleano
+
+null //Este es un valor placeholder
+undefined //Este es un valor placeholder
+
+//De la linea 1 a la linea 11 se les conoce como valores promitivos
+
+[1,2,3,] //Este es un valor tipo array
+{nombre: "Juan"} //Este es una valor tipo objeto
+
