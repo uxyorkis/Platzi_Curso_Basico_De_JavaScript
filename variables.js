19ebeaabc328bfa3ecab70bb15159709fb690ba8
uxyorkis <j.guerrero.morla@gmail.com>
2022-02-09 13:47:23 -0400

    se agregan el archivo de las clases sobre las variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+/* Variable es un lugar en memoria
+Es una representación de algún lugar en la memoria para guardar datos
+ */
+var nombre= "Yorkis";
+
+//Las variables tiene dos estados: 
+//•Declarar un variable
+Var edad;
+//•Inicializar esa variable
+edad = 27;
+
+//Tambien hay valores de string
+var elementos = ["computadora" , "celular"];
